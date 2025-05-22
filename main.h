92294831089d372c00be13501a95194ca1ccd888
PAVAN SADU <113853039+PAVANSADU17@users.noreply.github.com>
2025-05-22 09:54:06 +0530

    Create main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f1xx_hal.h"
+
+// Define GPIO Pins
+#define SEAT_SENSOR_GPIO    GPIOA
+#define SEAT_SENSOR_PIN     GPIO_PIN_0
+
+#define BELT_SENSOR_GPIO    GPIOA
+#define BELT_SENSOR_PIN     GPIO_PIN_1
+
+#define BUZZER_GPIO         GPIOA
+#define BUZZER_PIN          GPIO_PIN_5
+
+// Function Prototypes
+void SystemClock_Config(void);
+void Error_Handler(void);
+void MX_GPIO_Init(void);
+
+#endif /* __MAIN_H */
